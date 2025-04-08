33c3af7eb6d2db87804f7581364751f85c53d50a
karishma0903 <kkarishma1903@gmail.com>
2025-04-09 00:05:04 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 def handler(request):
     return {
         "statusCode": 200,
-        "headers": { "Content-Type": "application/json" },
-        "body": '{"message": "GDM API is live!"}'
+        "body": "GDM API is live!",
+        "headers": {
+            "Content-Type": "text/plain"
+        }
     }
